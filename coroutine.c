6281fd1fac1cf63c9c304216b2e79f15b2e365e8
bg2bkk <bg2bkk@gmail.com>
2016-10-15 18:51:09 +0800

    msgbuf

---
+++ coroutine.c
@@ -0,0 +1,22 @@
+
+#include "coroutine.h"
+
+struct coroutine *create_co( cfunc func, ucontext_t *uctx_main) {
+	struct coroutine *co = (struct coroutine *)malloc(sizeof(struct coroutine));
+	if( !co)
+		return NULL;
+	co->uctx.uc_stack.ss_sp = co->stack_;
+	co->uctx.uc_stack.ss_size = STACK_SIZE;
+	co->uctx.uc_link = uctx_main;
+	makecontext(&co->uctx, func, 0);
+
+	return co;
+}
+
+int free_co( struct coroutine *co) {
+	if(co && co->stack_)
+		free(co->stack_);
+	free(co);
+	return 1;
+}
+
