2ca211486ad753c78034ed06e58094c13390437c
bg2bkk <bg2bkk@gmail.com>
2016-10-15 17:52:42 +0800

    producer-consumer, with some bug

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <errno.h>
 #include <ucontext.h>
 
 #include "coroutine.h"
@@ -8,7 +10,7 @@
 	do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 struct msgbuf {
-	char buf[STACK_SIZE * 4];
+	char buf[STACK_SIZE ];
 	int len;
 	int pos;
 	int last;
@@ -21,11 +23,46 @@ struct msgbuf *buffer;
 
 void produce() {
 	while(1) {
+		printf("switch to producer\n");
+		if(buffer->last > 0) { // buffer not empty
+			printf("buffer->last > 0, produce but buffer not empty\n");
+			swapcontext(&producer->uctx, &consumer->uctx);
+		}
+		int n = read(0, buffer + buffer->last, buffer->len - buffer->last);
+		if(n < 0) {
+			// error
+		} else if (n == 0) { //ctrl + D
+			// switch to uctx_main, then end the process
+		} else {
+			// switch to consumer
+			buffer->last += n;
+			printf("producer %d bytes\n", n);
+			swapcontext(&producer->uctx, &consumer->uctx);
+		}
 	}
 }
 
 void consume() {
-
+	while(1) {
+		printf("switch to consumer\n");
+		if(buffer->last - buffer->pos > 0) { // not empty
+			int n = write(1, buffer + buffer->pos, buffer->last - buffer->pos);
+			if(n <= 0) {
+				printf("consumer error %s\n", strerror(errno));
+				// error and switch uctx_main, then end the process
+			} else {
+				if (n == buffer->last - buffer->pos) {
+					buffer->pos = buffer->last = 0;
+					swapcontext(&consumer->uctx, &producer->uctx);
+				} else {
+					buffer->pos += n;
+				}
+			}
+		} else {	// empty
+			printf("consume but empty\n");
+			swapcontext(&consumer->uctx, &producer->uctx);
+		}
+	}
 }
 
 int main()
@@ -46,19 +83,22 @@ int main()
 	buffer = (struct msgbuf *)malloc(sizeof(struct msgbuf));
 	if( !buffer)
 		handle_error("create msgbuf error");
+	buffer->pos = 0;
+	buffer->len = sizeof(buffer->buf);
+	buffer->last = 0;
 
 
 	getcontext(&producer->uctx);
 	producer->uctx.uc_stack.ss_sp = producer->stack_;
 	producer->uctx.uc_stack.ss_size = STACK_SIZE;
-//	producer->uctx.uc_link = &consumer->uctx;
-	producer->uctx.uc_link = &uctx_main;
+	producer->uctx.uc_link = &consumer->uctx;
+//	producer->uctx.uc_link = &uctx_main;
 
 	getcontext(&consumer->uctx);
 	consumer->uctx.uc_stack.ss_sp = consumer->stack_;
 	consumer->uctx.uc_stack.ss_size = STACK_SIZE;
-//	consumer->uctx.uc_link = &producer->uctx;
-	consumer->uctx.uc_link = &uctx_main;
+	consumer->uctx.uc_link = &producer->uctx;
+//	consumer->uctx.uc_link = &uctx_main;
 
 	makecontext(&(producer->uctx), produce, 0);
 	makecontext(&(consumer->uctx), consume, 0);
